739b5b653761be1c4979ccf875152481d4ecae31
Valentina <valush31@gmail.com>
2023-01-11 22:10:47 +0300

    add Excersize 30

---
+++ Excersize_30/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и 
+// далее выводит массив на экран в одну строку.
+
+int [] number = new int[8];
+Console.Write("(");
+
+for (int i = 0; i < number.Length; i++)
+ {
+    number [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write(")");
+
+int Method (int a)
+{
+    return number[a];
+}
